a391840b2faaebfb60d6ef44ce9483b062d4d3e2
Marco Lonati <164371172+PedroDLQ@users.noreply.github.com>
2025-09-23 17:28:57 +0200

    Initial project layout

---
+++ gb_emulator/target/debug/deps/gb_emulator.d
@@ -0,0 +1,5 @@
+C:\Users\user\.rustup\rust.projects\gb_emulator\target\debug\deps\gb_emulator.exe: src/main.rs
+
+C:\Users\user\.rustup\rust.projects\gb_emulator\target\debug\deps\gb_emulator.d: src/main.rs
+
+src/main.rs:
---
+++ gb_emulator/target/debug/gb_emulator.d
@@ -0,0 +1,1 @@
+C:\Users\user\.rustup\rust.projects\gb_emulator\target\debug\gb_emulator.exe: C:\Users\user\.rustup\rust.projects\gb_emulator\src\main.rs
