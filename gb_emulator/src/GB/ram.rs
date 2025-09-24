b8f27d8634d78d94ed9beb55f5b344735166bb23
PedroDLQ <marcolizzi2003@gmail.com>
2025-09-24 16:23:41 +0200

    RAM initial implementation

--- gb_emulator/src/GB/ram.rs
+++ gb_emulator/src/GB/ram.rs
@@ -1,1 +1,22 @@
- 
+pub struct Ram {
+
+    memory: [u8; 65536], // Max memory size of the Game Boy system (65.536 kB)
+}
+
+impl Ram {
+
+    pub fn new() -> self { // Struct constructor
+
+        Ram { memory: [0; 65536] } 
+    }
+
+    pub fn read(&self, address: u16) -> u8 {
+
+        self.memory[address as usize]
+    }
+
+    pub fn write(&mut self, address: u16, value: u8) {
+
+        self.memory[address as usize] = value;
+    }
+}
